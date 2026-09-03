31cedb6522f7ff552196ba571674fbbfcf6c5bb7
Memo-Player <memoplayer40@gmail.com>
2026-09-03 10:43:01 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Firebase Web Config
 // Replace the values below with the config from Firebase Console.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAKDinGLySAJgEAczZ7zdZt3p22rpj-E6o",
+  authDomain: "memoweb40.firebaseapp.com",
+  projectId: "memoweb40",
+  storageBucket: "memoweb40.firebasestorage.app",
+  messagingSenderId: "704770837742",
+  appId: "1:704770837742:web:095519f2c639a7fb3877ff",
+  measurementId: "G-MW9K98XXY3"
 };
