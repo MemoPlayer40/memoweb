00130728a8eb27e8fcaaf83bc5dace7f8e288eba
Memo-Player <memoplayer40@gmail.com>
2026-09-03 11:17:24 +0300

    Delete discord-auth-config.js

--- discord-auth-config.js
+++
@@ -1,5 +0,0 @@
-import { firebaseConfig } from "./firebase-config.js";
-
-// The backend is deployed to the same Firebase project.
-export const discordAuthUrl =
-  `https://us-central1-${firebaseConfig.projectId}.cloudfunctions.net/discordAuth`;
